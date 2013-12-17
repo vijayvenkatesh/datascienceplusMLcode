83f3d90a3df81faa39548553a610ac6be0e1d304
vijay <vijayv@pobox.com>
2013-12-16 23:45:48 -0500

    Finished extra credit

--- salaries/salaries.py
+++ salaries/salaries.py
@@ -1,13 +1,32 @@
+import sys
+
 def average(salaries):
+	playerIdNames = names(salaries.keys()) # Make a call to the names function to get the real list of names
+	
 	for key, value in salaries.items():
 		x = 0.0
 		for pay in value:
 			x = x + pay
-		print key, x/len(value), value
+		print playerIdNames[key], x/len(value)
+
+# This function takes the list of names and returns a dictionary of playerId: "FirstName + LastName"
+def names( keys = []):
+	IdToNames = {}
+	master = sys.argv[2]
+	with open(master) as f:
+		next(f)
+		for line in f:
+			lines = line.split(",")
+			playerId = lines[1]
+			fname = lines[16]
+			lname = lines[17]
+			IdToNames[playerId] = fname + " " + lname
+	return IdToNames
 
 
 avg = {}
-with open('/Users/vjbu/Google Drive/classes/datascience/data/Salaries.csv') as f:
+salary = sys.argv[1]
+with open(salary) as f:
 	next(f)
 	for l in f:
 		lines = l.split(",")
